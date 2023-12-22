12e1b578f0ca63cb4ae5aed42c8bf5274f7593b0
mistrmochov <142264185+mistrmochov@users.noreply.github.com>
2023-12-22 02:11:54 +0100

    Improved script syntax, more files to remove

--- magiskremove.sh
+++ magiskremove.sh
@@ -1,5 +1,20 @@
-echo "Do you really want to uninstall Magisk Delta from waydroid?(y/n)"
-read a
+loope=true
+a=""
+while [ $loope = true ]
+do
+read -p "Do you really want to uninstall Magisk Delta from waydroid?(y/n)" a
+if [ $a = "y" ]
+then
+loope=false
+elif [ $a = "n" ]
+then
+loope=false
+else
+clear
+loope=true
+echo "No option selected, try again!"
+fi
+done
 if [ $a = "y" ]
 then
 waydroid session stop
@@ -17,7 +32,13 @@ sudo rm -rf ~/.local/share/waydroid/data/data/io.github.huskydg.magisk.png
 sudo rm -rf /var/lib/waydroid/overlay/sbin/*
 sudo rm -rf /var/lib/waydroid/overlay/system/etc/init/bootanim.rc
 sudo gzip -dk /var/lib/waydroid/overlay/system/etc/init/bootanim.rc.gz
+sudo rm -rf /var/lib/waydroid/overlay/system/etc/init/magisk
+sudo rm -rf /var/lib/waydroid/overlay/system/addon.d
 sudo rm -rf /var/lib/waydroid/overlay_rw/system/sbin/.magisk
+sudo rm -rf /var/lib/waydroid/overlay_rw/system/system/etc/init/bootanim.rc
+sudo rm -rf /var/lib/waydroid/overlay_rw/system/system/etc/init/bootanim.rc.gz
+sudo rm -rf /var/lib/waydroid/overlay_rw/system/system/etc/init/magisk 
+sudo rm -rf /var/lib/waydroid/overlay_rw/system/system/addon.d
 sudo systemctl start waydroid-container.service
 echo "Magisk removed, goodbye!"
 else
